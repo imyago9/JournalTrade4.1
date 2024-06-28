fc7a3469685ed3f717ee8d67e7ac25ff8249ec3b
imyago9 <yagofarhat555@gmail.com>
2024-06-28 16:34:54 -0400

    now???

--- InstallerUpdater.py
+++ InstallerUpdater.py
@@ -53,8 +53,11 @@ def create_update_script():
 echo Starting update...
 ping 127.0.0.1 -n 5 > nul
 xcopy /s /y "%~dp0new_files\\*" "%~dp0"
+echo Copied new files
 rmdir /s /q "%~dp0new_files"
+echo Removed new_files folder.
 start "" "%~dp0JournalTrade.exe"
+echo Starting application.
 exit
 ''')
 
